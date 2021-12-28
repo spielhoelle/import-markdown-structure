a922f10c8f3ba7b57a0476d4d4cca5b8b3e218ba
Thomas Kuhnert <spielhoelle@posteo.net>
2021-12-28 10:46:28 +0700

    feat: modularize functions and setup action-scheduler

--- options.php
+++ options.php
@@ -50,6 +50,22 @@ $max_file_download = 5;
 //         wp_delete_term($type->term_id, 'archive-category');
 //     }
 // }
+require_once(plugin_dir_path(__FILE__) . '/action-scheduler/action-scheduler.php');
+
+function tmy_schedule_download_parse_task()
+{
+    if (false === as_has_scheduled_action('tmy_shedule_background_task')) {
+        as_schedule_recurring_action(time() + 5, MINUTE_IN_SECONDS, 'tmy_shedule_background_task');
+    }
+}
+add_action('init', 'tmy_schedule_download_parse_task');
+
+function tmy_schedule_download_parse_pdfs()
+{
+    file_put_contents('php://stdout', 'Background task ran ' . time(), FILE_APPEND);
+    download_files_from_content_links();
+}
+add_action('tmy_shedule_background_task', 'tmy_schedule_download_parse_pdfs');
 add_filter('wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes) {
     $filetype = wp_check_filetype($filename, $mimes);
     return [
@@ -154,165 +170,13 @@ function tmy_markdown_handle_post()
             echo "\n\rFile upload, import of category structure and post-creation successful!";
         }
     } else if ($_POST && isset($_POST['download_files_from_content_links'])) {
-        $allposts = get_posts(array('post_type' => 'archive', 'numberposts' => -1));
-        $i = 0;
-        foreach ($allposts as $eachpost) {
-            $post_content_links = wp_extract_urls($eachpost->post_content);
-            if (count($post_content_links) > 0) {
-                foreach ($post_content_links as $post_content_link) {
-                    $basename = basename($post_content_link);
-                    $basefilename = urldecode(preg_replace('/\?.*/', '', $basename));
-                    $filename = preg_replace('/#[^?|^&]*/', '', $basefilename);
-                    $file_parts = pathinfo($filename);
-                    if (
-                        !post_exists($filename)
-                        && $filename !== 'edit'
-                        && $filename !== 'view'
-                        // && isset($file_parts["extension"]) && $file_parts['extension'] === 'docx'
-                    ) {
-                        if ($i < $max_file_download) {
-                            $query = parse_url($post_content_link, PHP_URL_QUERY);
-                            if ($query) {
-                                $post_content_link .= '&raw=1&dl=1';
-                            } else {
-                                $post_content_link .= '?raw=1&dl=1';
-                            }
-                            $post_content_link = str_replace('dl=0', '', $post_content_link);
-                            $contents = file_get_contents($post_content_link, false, stream_context_create(['http' => ['ignore_errors' => true]]));
-                            // var_dump($http_response_header);
-                            $post_id = $eachpost->ID;
-                            $filetype = wp_check_filetype($filename, null);
-                            $wp_upload_dir = wp_upload_dir();
-
-                            $new_file_path = $wp_upload_dir['path'] . '/' . $filename;
-                            if (!file_exists($filename)) {
-                                file_put_contents($new_file_path, $contents);
-                            }
-                            $attachment = array(
-                                'guid'           => $new_file_path,
-                                'post_mime_type' => $filetype['type'],
-                                'post_title'     => $filename,
-                                'post_status'    => 'inherit',
-                                'post_content'   => "placeholder",
-                                'post_excerpt'   => $post_content_link,
-                            );
-                            $attachment_id = wp_insert_attachment($attachment, $new_file_path, $post_id);
-                            require_once(ABSPATH . 'wp-admin/includes/image.php');
-                            $attach_data = wp_generate_attachment_metadata($attachment_id, $new_file_path);
-                            wp_update_attachment_metadata($attachment_id, $attach_data);
-                            $i++;
-                        }
-                    } else {
-                        // echo "\n\rPOST is already existing";
-                    }
-                }
-            }
-        }
+        download_files_from_content_links();
     } else if ($_POST && isset($_POST['get_pdf_textcontent'])) {
-        $post = get_post($_POST['get_pdf_textcontent']);
-        $filepath = get_attached_file($post->ID);
-        $text_content = "";
-        // Parse pdf file and build necessary objects.
-        $file_parts = pathinfo($filepath);
-        if ($file_parts['extension'] === 'pdf') {
-            $parser = new \Smalot\PdfParser\Parser();
-            $pdf    = $parser->parseFile($filepath);
-            $text_content = trim(preg_replace('/\s\s+/', ' ', $pdf->getText()));
-            // Retrieve all details from the pdf file.
-            // $details  = $pdf->getDetails();
-            // Loop over each property to extract values (string or array).
-            // $meta_accumulation = $filepath . "\n";
-            // foreach ($details as $property => $value) {
-            //     if (is_array($value)) {
-            //         $value = implode(', ', $value);
-            //     }
-            //     $meta_accumulation += $property . ' => ' . $value . "\n";
-            // }
-            // $details  = $pdf->getDetails();
-            // $meta = "Meta: \n\r";
-            // foreach ($details as $property => $value) {
-            //     if (is_array($value)) {
-            //         $value = implode(', ', $value);
-            //     }
-            //     $meta .= $property . ' => ' . $value . "\n";
-            // }
-            // $result = $meta . "\n\rTextcontent: \n\r" . str_replace("\t", '', $text_content);
-
-        }
-        if ($file_parts['extension'] === 'docx') {
-            $content = '';
-            $zip = zip_open($filepath);
-            if (!$zip || is_numeric($zip)) return false;
-            while ($zip_entry = zip_read($zip)) {
-                if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
-                if (zip_entry_name($zip_entry) != "word/document.xml") continue;
-                $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
-                zip_entry_close($zip_entry);
-            } // end while
-            zip_close($zip);
-            $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
-            $content = str_replace('</w:r></w:p>', "\r\n", $content);
-            $text_content = strip_tags($content);
-        }
-        $postid = $post->ID;
-        $result = str_replace("\t", '', trim($text_content));
-        $updated_post = array(
-            'ID' => $postid,
-            'post_content' => $result,
-            // 'post_excerpt' => "replaced by AI summary"
-        );
-        $err = wp_update_post($updated_post, true, true);
-        if (is_wp_error($err)) {
-            var_dump($err);
-            error_log(print_r($err, 1));
-        } else {
-            echo $result;
-            die;
-        }
+        $id = $_POST['get_pdf_textcontent'];
+        get_pdf_textcontent($id);
     } else if ($_POST && isset($_POST['tmy_send_to_ai'])) {
-        $post = get_post($_POST['tmy_send_to_ai']);
-        $trimmed = trim(preg_replace('/\s\s+/', ' ', $post->post_content));
-        $trimmed = preg_replace('/\d{1,2}\s{1}/', "", $trimmed);
-        $max_tokens = 64;
-        preg_match('/[A-Z]{1}[a-z]*\s[a-z]*\s[a-z]*\s[a-z]*\s[a-z]*\s[^\.]*\./', $trimmed, $indexOfFirstSentence, PREG_OFFSET_CAPTURE);
-        $shortened = substr($trimmed, $indexOfFirstSentence && $indexOfFirstSentence[0] ? $indexOfFirstSentence[0][1] : 0, 2049 - $max_tokens);
-        $tldr = $shortened . "\n\ntl;dr:\n";
-        $args = array(
-            'headers' => array(
-                'Content-Type' => 'application/json',
-                'Authorization' => 'Bearer ' . get_option('tmy_open_ai_api_key'),
-            ),
-            "body" => json_encode(array(
-                "prompt" => $tldr,
-                "temperature" => 0.7,
-                "max_tokens" => $max_tokens,
-                "top_p" => 1,
-                "frequency_penalty" => 0.2,
-                "presence_penalty" => 0,
-                "stop" => ["\n\ntl;dr:\n"]
-            ))
-        );
-        $response = wp_remote_post('https://api.openai.com/v1/engines/davinci/completions', $args);
-        if (is_wp_error($response)) {
-            $error_message = $response->get_error_message();
-            echo "Something went wrong: $error_message";
-            die;
-        }
-        $body = wp_remote_retrieve_body($response);
-        $json = json_decode($body);
-        $post_data = "";
-
-        foreach ($json->choices as $choice) {
-            $post_data .= $choice->text . "\n\r";
-        }
-        $data = array(
-            'ID' => $post->ID,
-            'post_excerpt' => $post_data
-        );
-        // var_dump('$data' . $data);
-        $res = wp_update_post($data, true);
-        echo $post_data;
-        die;
+        $id = $_POST['tmy_send_to_ai'];
+        tmy_send_to_ai($id);
     }
 };
 add_action('add_meta_boxes', function () {
@@ -328,8 +192,180 @@ add_action('add_meta_boxes', function () {
         echo '</ul>';
     }, 'archive');
 });
+function get_pdf_textcontent($id)
+{
+    $post = get_post($id);
+    $filepath = get_attached_file($post->ID);
+    $text_content = "";
+    // Parse pdf file and build necessary objects.
+    $file_parts = pathinfo($filepath);
+    if ($file_parts['extension'] === 'pdf') {
+        $parser = new \Smalot\PdfParser\Parser();
+        $pdf    = $parser->parseFile($filepath);
+        $text_content = trim(preg_replace('/\s\s+/', ' ', $pdf->getText()));
+        // Retrieve all details from the pdf file.
+        // $details  = $pdf->getDetails();
+        // Loop over each property to extract values (string or array).
+        // $meta_accumulation = $filepath . "\n";
+        // foreach ($details as $property => $value) {
+        //     if (is_array($value)) {
+        //         $value = implode(', ', $value);
+        //     }
+        //     $meta_accumulation += $property . ' => ' . $value . "\n";
+        // }
+        // $details  = $pdf->getDetails();
+        // $meta = "Meta: \n\r";
+        // foreach ($details as $property => $value) {
+        //     if (is_array($value)) {
+        //         $value = implode(', ', $value);
+        //     }
+        //     $meta .= $property . ' => ' . $value . "\n";
+        // }
+        // $result = $meta . "\n\rTextcontent: \n\r" . str_replace("\t", '', $text_content);
 
+    }
+    if ($file_parts['extension'] === 'docx') {
+        $content = '';
+        $zip = zip_open($filepath);
+        if (!$zip || is_numeric($zip)) return false;
+        while ($zip_entry = zip_read($zip)) {
+            if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
+            if (zip_entry_name($zip_entry) != "word/document.xml") continue;
+            $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+            zip_entry_close($zip_entry);
+        } // end while
+        zip_close($zip);
+        $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
+        $content = str_replace('</w:r></w:p>', "\r\n", $content);
+        $text_content = strip_tags($content);
+    }
+    $postid = $post->ID;
+    $result = str_replace("\t", '', trim($text_content));
+    $updated_post = array(
+        'ID' => $postid,
+        'post_content' => $result,
+        // 'post_excerpt' => "replaced by AI summary"
+    );
+    $err = wp_update_post($updated_post, true, true);
+    if (is_wp_error($err)) {
+        var_dump($err);
+        error_log(print_r($err, 1));
+    } else {
+        echo $result;
+        die;
+    }
+}
+function download_files_from_content_links()
+{
+    global $max_file_download;
+    if (!function_exists('post_exists')) {
+        require_once(ABSPATH . 'wp-admin/includes/post.php');
+    }
+    if (!function_exists('wp_get_current_user')) {
+        include(ABSPATH . "wp-includes/pluggable.php");
+    }
+    $allposts = get_posts(array('post_type' => 'archive', 'numberposts' => -1));
+    $i = 0;
+    foreach ($allposts as $eachpost) {
+        $post_content_links = wp_extract_urls($eachpost->post_content);
+        if (count($post_content_links) > 0) {
+            foreach ($post_content_links as $post_content_link) {
+                $basename = basename($post_content_link);
+                $basefilename = urldecode(preg_replace('/\?.*/', '', $basename));
+                $filename = preg_replace('/#[^?|^&]*/', '', $basefilename);
+                $file_parts = pathinfo($filename);
+                if (
+                    !post_exists($filename)
+                    && $filename !== 'edit'
+                    && $filename !== 'view'
+                    // && isset($file_parts["extension"]) && $file_parts['extension'] === 'docx'
+                ) {
+                    if ($i < $max_file_download) {
+                        $query = parse_url($post_content_link, PHP_URL_QUERY);
+                        if ($query) {
+                            $post_content_link .= '&raw=1&dl=1';
+                        } else {
+                            $post_content_link .= '?raw=1&dl=1';
+                        }
+                        $post_content_link = str_replace('dl=0', '', $post_content_link);
+                        $contents = file_get_contents($post_content_link, false, stream_context_create(['http' => ['ignore_errors' => true]]));
+                        // var_dump($http_response_header);
+                        $post_id = $eachpost->ID;
+                        $filetype = wp_check_filetype($filename, null);
+                        $wp_upload_dir = wp_upload_dir();
 
+                        $new_file_path = $wp_upload_dir['path'] . '/' . $filename;
+                        if (!file_exists($filename)) {
+                            file_put_contents($new_file_path, $contents);
+                        }
+                        $attachment = array(
+                            'guid'           => $new_file_path,
+                            'post_mime_type' => $filetype['type'],
+                            'post_title'     => $filename,
+                            'post_status'    => 'inherit',
+                            'post_content'   => "placeholder",
+                            'post_excerpt'   => $post_content_link,
+                        );
+                        $attachment_id = wp_insert_attachment($attachment, $new_file_path, $post_id);
+                        require_once(ABSPATH . 'wp-admin/includes/image.php');
+                        $attach_data = wp_generate_attachment_metadata($attachment_id, $new_file_path);
+                        wp_update_attachment_metadata($attachment_id, $attach_data);
+                        $i++;
+                    }
+                } else {
+                    // echo "\n\rPOST is already existing";
+                }
+            }
+        }
+    }
+}
+
+function tmy_send_to_ai($id)
+{
+    $post = get_post($id);
+    $trimmed = trim(preg_replace('/\s\s+/', ' ', $post->post_content));
+    $trimmed = preg_replace('/\d{1,2}\s{1}/', "", $trimmed);
+    $max_tokens = 64;
+    preg_match('/[A-Z]{1}[a-z]*\s[a-z]*\s[a-z]*\s[a-z]*\s[a-z]*\s[^\.]*\./', $trimmed, $indexOfFirstSentence, PREG_OFFSET_CAPTURE);
+    $shortened = substr($trimmed, $indexOfFirstSentence && $indexOfFirstSentence[0] ? $indexOfFirstSentence[0][1] : 0, 2049 - $max_tokens);
+    $tldr = $shortened . "\n\ntl;dr:\n";
+    $args = array(
+        'headers' => array(
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . get_option('tmy_open_ai_api_key'),
+        ),
+        "body" => json_encode(array(
+            "prompt" => $tldr,
+            "temperature" => 0.7,
+            "max_tokens" => $max_tokens,
+            "top_p" => 1,
+            "frequency_penalty" => 0.2,
+            "presence_penalty" => 0,
+            "stop" => ["\n\ntl;dr:\n"]
+        ))
+    );
+    $response = wp_remote_post('https://api.openai.com/v1/engines/davinci/completions', $args);
+    if (is_wp_error($response)) {
+        $error_message = $response->get_error_message();
+        echo "Something went wrong: $error_message";
+        die;
+    }
+    $body = wp_remote_retrieve_body($response);
+    $json = json_decode($body);
+    $post_data = "";
+
+    foreach ($json->choices as $choice) {
+        $post_data .= $choice->text . "\n\r";
+    }
+    $data = array(
+        'ID' => $post->ID,
+        'post_excerpt' => $post_data
+    );
+    // var_dump('$data' . $data);
+    $res = wp_update_post($data, true);
+    echo $post_data;
+    die;
+}
 add_action('add_meta_boxes', 'tmy_add_your_meta_box2');
 function tmy_add_your_meta_box2()
 {
